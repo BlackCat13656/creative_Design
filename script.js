896388ff098e30c5d9e761405761fdcbf9b5c9b1
Ekan <ekanessien@gmail.com>
2025-05-30 19:09:40 -0700

    Advances in the creation and styling of the team section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 document.querySelectorAll(".story-btn").forEach((btn) => {
   btn.addEventListener("click", () => {
-    console.log("Clicked");
+    btn.classList.toggle("change");
+    btn.nextElementSibling.classList.toggle("change");
   });
 });
